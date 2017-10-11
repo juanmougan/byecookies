7beee88c9fa939cac739d329eb531c50a351f84d
Juan Manuel Mougan <juanmougan@gmail.com>
2017-10-11 17:20:12 -0300

    Started with cookie deletion

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,28 @@
 'use strict';
-
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+ 
+// TODO adapt this to delete cookies
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var currentTime = new Date();
+        /*
+        Here I should instead do something like
+        if(isBlacklistedUrl(url)) {
+			deleteCookiesForSite(url);
+			navigateToSite();
+        }
+        */
+        if(isOfficeTime(currentTime) && isWeekday(currentTime)){
+            return {redirectUrl: chrome.extension.getURL('index.html')};    
+        }
+        return details.url;
+    },
+    {
+        urls: [
+            "*://*.facebook.com/*",
+            "*://*.twitter.com/*",
+            "*://*.gmail.com/*",
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
