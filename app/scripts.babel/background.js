13fffb6258149da0fca3e1d058bd6bdeeea3584c
Juan Manuel Mougan <juanmougan@gmail.com>
2017-10-11 22:31:24 -0300

    Delete cookies logic added

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,26 +1,19 @@
 'use strict';
- 
+
 // TODO adapt this to delete cookies
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var currentTime = new Date();
-        /*
-        Here I should instead do something like
-        if(isBlacklistedUrl(url)) {
-			deleteCookiesForSite(url);
-			navigateToSite();
+      const URL = 'www.lanacion.com.ar'
+      chrome.cookies.getAll({domain: URL}, function(cookies) {
+        for(var i=0; i<cookies.length;i++) {
+          chrome.cookies.remove({url: "http://" + URL + cookies[i].path, name: cookies[i].name});
         }
-        */
-        if(isOfficeTime(currentTime) && isWeekday(currentTime)){
-            return {redirectUrl: chrome.extension.getURL('index.html')};    
-        }
+      });
         return details.url;
     },
     {
         urls: [
-            "*://*.facebook.com/*",
-            "*://*.twitter.com/*",
-            "*://*.gmail.com/*",
+            "*://*.lanacion.com.ar/*"
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
